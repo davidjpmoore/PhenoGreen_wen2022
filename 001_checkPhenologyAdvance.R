9dd815d3f43d9d60c77fdf18c84a1295c376588a
davidjpmoore <davidjpmoore@email.arizona.edu>
2022-07-05 17:06:11 -0600

    Adding an analysis that assumes only an increase in LAI but NO CHANGE in the timing of spring or fall.

--- 001_checkPhenologyAdvance.R
+++ 001_checkPhenologyAdvance.R
@@ -1,4 +1,7 @@
 library(tidyr)
+library(dplyr)
+library(ggplot2)
+
 LAI_SpringAdvancewide=read.csv(file="data/SpringAdvanceOnly.csv",header = TRUE)
 
 LAISpringAdvance_slim <-
@@ -24,6 +27,39 @@ TrendLAI = LAISpringAdvance_slim %>%
   summarise_at(vars(LAI), list(LAImean = mean))
 
 library(ggplot2)
+
+p <- ggplot(TrendLAI, aes(YEAR, LAImean))
+# A basic scatter plot
+p + geom_point(aes(colour = factor(SEASON)), size = 4)
+
+
+# GROWTH STIMULATION ONLY 
+
+StimOnlywide=read.csv(file="data/GrowthStimOnly.csv",header = TRUE)
+
+StimOnly_slim <-
+  pivot_longer(data= StimOnlywide,
+               cols = starts_with("LAI"),
+               names_to = "YEARind",
+               values_to = "LAI"
+  )
+
+StimOnly_slim$YEAR= as.numeric(substr(StimOnly_slim$YEARind, start = 4, stop = 7))
+StimOnly_slim$SEASON="none"
+StimOnly_slim$SEASON[StimOnly_slim$day>90]="early"
+StimOnly_slim$SEASON[StimOnly_slim$day>150]="mid"
+StimOnly_slim$SEASON[StimOnly_slim$day>200]="late"
+StimOnly_slim$SEASON[StimOnly_slim$day>250]="none"
+
+plot(StimOnly_slim$YEAR,StimOnly_slim$LAI)
+
+bs <- ggplot(StimOnly_slim, aes(day, LAI))
+bs + geom_point(aes(colour = factor(YEAR)), size = 4)
+
+TrendLAI = StimOnly_slim %>%
+  group_by(YEAR,SEASON) %>%
+  summarise_at(vars(LAI), list(LAImean = mean))
+
 
 p <- ggplot(TrendLAI, aes(YEAR, LAImean))
 # A basic scatter plot
